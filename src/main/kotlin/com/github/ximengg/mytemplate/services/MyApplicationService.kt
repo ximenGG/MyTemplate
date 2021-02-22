85c3a4a602ce2095d8c20fe8489865bbb1d2cc82
GitHub Action <action@github.com>
2021-02-22 05:33:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/ximengg/mytemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ximengg/mytemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ximengg.mytemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ximengg.mytemplate.MyBundle
 
 class MyApplicationService {
 
